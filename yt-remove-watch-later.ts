2deea1b66e3286c9bfed049586cab867d8cb089a
Ali Hamad <115514687+ah410@users.noreply.github.com>
2025-09-07 01:44:59 -0700

    chore: initial typescript setup

---
+++ yt-remove-watch-later.ts
@@ -0,0 +1,17 @@
+// 1. Load the Watch Later page
+
+
+// 2. Grab the set of visible videos (no extra scroll)
+      // Find the action menu button that is within ytd-playlist-video-renderer HTML element
+      // Click it
+
+
+// 3. Remove them one by one
+      // Find the remove watch later HTML element
+      // Click it
+
+
+// 4. Once count < 1, reload the page
+
+
+// 5. Repeat until no more videos left
